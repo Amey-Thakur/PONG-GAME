49b2d7946aebb4a40cb9998ab4cbe0c0f7867da5
Amey Thakur <ameythakur20@gmail.com>
2026-01-19 16:09:38 -0500

    PONG-GAME

--- Source Code/main.py
+++ Source Code/main.py
@@ -174,7 +174,7 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen_width = 1280
-screen_height = 960
+screen_height = 720
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('AMEY & MEGA')
 
